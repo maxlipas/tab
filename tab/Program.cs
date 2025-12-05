e5e55f096bdc8c19a48fd624a84353d2760db6cd
Максим Липский <lipskiy@kbkit.pri>
2025-12-05 09:43:56 +0200

    first commit

---
+++ tab/Program.cs
@@ -0,0 +1,23 @@
+﻿try
+{
+    double x = 1;
+    double k = 1;
+    double y = 1;
+    double h = 0.1;
+    do
+    {
+        for (double t = 2 * k; t > 0; t--)
+        {
+            y = t * y;
+        }
+        double f = (Math.Pow(x, 2 * k)) / y;
+        Console.WriteLine($"f({x}) = {f}");
+        x += h;
+    }
+    while (x < 2.1);
+
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
